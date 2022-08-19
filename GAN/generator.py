24b6749b11b4b663d3dd48a18161120b38a8848a
reza <shahabian.reza@gmail.com>
2022-08-19 11:45:35 +0100

    add generator block and test it in the main

---
+++ GAN/generator.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: get_generator_block
+def get_generator_block(input_dim, output_dim):
+    '''
+    Function for returning a block of the generator's neural network
+    given input and output dimensions.
+    Parameters:
+        input_dim: the dimension of the input vector, a scalar
+        output_dim: the dimension of the output vector, a scalar
+    Returns:
+        a generator neural network layer, with a linear transformation
+          followed by a batch normalization and then a relu activation
+    '''
+    return nn.Sequential(
+        # Hint: Replace all of the "None" with the appropriate dimensions.
+        # The documentation may be useful if you're less familiar with PyTorch:
+        # https://pytorch.org/docs/stable/nn.html.
+        #### START CODE HERE ####
+        nn.Linear(input_dim, output_dim),
+        nn.BatchNorm1d(output_dim),
+        #### END CODE HERE ####
+        nn.ReLU(inplace=True)
+    )
+
---
+++ GAN/main.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST  # Training dataset
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+from generator import get_generator_block
+
+torch.manual_seed(0)  # Set for testing purposes, please do not change!
+
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in a uniform grid.
+    '''
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+
+# Verify the generator block function
+def test_gen_block(in_features, out_features, num_test=1000):
+    block = get_generator_block(in_features, out_features)
+
+    # Check the three parts
+    assert len(block) == 3
+    assert type(block[0]) == nn.Linear
+    assert type(block[1]) == nn.BatchNorm1d
+    assert type(block[2]) == nn.ReLU
+
+    # Check the output shape
+    test_input = torch.randn(num_test, in_features)
+    test_output = block(test_input)
+    assert tuple(test_output.shape) == (num_test, out_features)
+    assert test_output.std() > 0.55
+    assert test_output.std() < 0.65
+
+
+test_gen_block(25, 12)
+test_gen_block(15, 28)
+print("Success!")
