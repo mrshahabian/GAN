a016c644a1d3d004223ab8e969159aae3e29b98a
reza <shahabian.reza@gmail.com>
2022-08-19 12:38:12 +0100

    add generator class and test it in the main

--- GAN/generator.py
+++ GAN/generator.py
@@ -24,3 +24,47 @@ def get_generator_block(input_dim, output_dim):
         nn.ReLU(inplace=True)
     )
 
+
+# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: Generator
+class Generator(nn.Module):
+    '''
+    Generator Class
+    Values:
+        z_dim: the dimension of the noise vector, a scalar
+        im_dim: the dimension of the images, fitted for the dataset used, a scalar
+          (MNIST images are 28 x 28 = 784 so that is your default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+
+    def __init__(self, z_dim=10, im_dim=784, hidden_dim=128):
+        super(Generator, self).__init__()
+        # Build the neural network
+        self.gen = nn.Sequential(
+            get_generator_block(z_dim, hidden_dim),
+            get_generator_block(hidden_dim, hidden_dim * 2),
+            get_generator_block(hidden_dim * 2, hidden_dim * 4),
+            get_generator_block(hidden_dim * 4, hidden_dim * 8),
+            # There is a dropdown with hints if you need them!
+            #### START CODE HERE ####
+            nn.Linear(hidden_dim * 8, im_dim),
+            nn.Sigmoid()
+            #### END CODE HERE ####
+        )
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor,
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        return self.gen(noise)
+
+    # Needed for grading
+    def get_gen(self):
+        '''
+        Returns:
+            the sequential model
+        '''
+        return self.gen
--- GAN/main.py
+++ GAN/main.py
@@ -7,7 +7,7 @@ from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
-from generator import get_generator_block
+from generator import get_generator_block, Generator
 
 torch.manual_seed(0)  # Set for testing purposes, please do not change!
 
@@ -43,4 +43,29 @@ def test_gen_block(in_features, out_features, num_test=1000):
 
 test_gen_block(25, 12)
 test_gen_block(15, 28)
-print("Success!")
+print(" Generator Block Success!")
+
+
+# Verify the generator class
+def test_generator(z_dim, im_dim, hidden_dim, num_test=10000):
+    gen = Generator(z_dim, im_dim, hidden_dim).get_gen()
+    # print(gen)
+
+    # Check there are six modules in the sequential part
+    assert len(gen) == 6
+    test_input = torch.randn(num_test, z_dim)
+    test_output = gen(test_input)
+
+    # Check that the output shape is correct
+    assert tuple(test_output.shape) == (num_test, im_dim)
+    assert test_output.max() < 1, "Make sure to use a sigmoid"
+    assert test_output.min() > 0, "Make sure to use a sigmoid"
+    assert test_output.min() < 0.5, "Don't use a block in your solution"
+    assert test_output.std() > 0.05, "Don't use batchnorm here"
+    assert test_output.std() < 0.15, "Don't use batchnorm here"
+
+
+test_generator(5, 10, 20)
+test_generator(20, 8, 24)
+print("Generator class Success!")
+
